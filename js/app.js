2a6b80b42111c96b6d2eebe1b080e788341d458b
JhordanReyes <reyesjhordan4@gmail.com>
2022-04-19 13:50:00 -0500

    Se agregó una pagina

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/**UN BESO APACIONADO
+    COMPRAR PAPITAS PICANTES
+    COMER UN LIMON
+    CANTAR */
+
+function retos(numero){
+    switch(numero){
+        case 1:
+            let salida = document.getElementById("reto").innerText = "RETO 1: UN BESO APACIONADO";
+            return salida;
+        case 2:
+            salida = document.getElementById("reto").innerText = "RETO 2: COMPRAR PAPITAS PICANTES";
+                return salida;
+        case 3:
+            salida = document.getElementById("reto").innerText = "RETO 3: COMER UN LIMON";
+            return salida;
+        case 4:
+            salida = document.getElementById("reto").innerText = "RETO 4: CANTAR";
+            return salida;
+    }
+}
+function ruleta(){
+    let numeroRamdom = parseInt(Math.random()*4);
+    numeroRamdom += 1;
+    salida = retos(numeroRamdom);
+    document.getElementById("reto").innerText = salida;
+}
